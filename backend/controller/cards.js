e4526ccddfddb7763b1bfc92bb96b8870d6d59f4
AkshayRajeev95 <46608572+AkshayRajeev95@users.noreply.github.com>
2020-11-23 22:31:26 -0600

    Added post for cards

--- backend/controller/cards.js
+++ backend/controller/cards.js
@@ -29,6 +29,41 @@ const getCard = asyncHandler(async (req, res, next) => {
     return next(res.status(200).json({ success: true, data: card }));
 });
 
-export { getCard };
+/*
+ * @desc     Create card in Db
+ * @route    GET /api/v1/cards/
+ * @access   Public
+ */
 
-//
+const createCard = asyncHandler(async (req, res, next) => {
+    // Validate Body is not empty
+    if (!req.body.userId || !req.body.cardNumbaer) {
+        return next(
+            res.status(400).send({
+                message: 'Content can not be empty!',
+            })
+        );
+    }
+
+    const card = await Cards.create({
+        cardNumbaer: req.body.cardNumbaer,
+    });
+
+    //I should be validating user itself right ? 
+    if (!card || card.length == 0) {
+        console.log(`Card Not Found with id ${res}`);
+        return res.status(500).json({
+            success: false,
+            error: `"Some error occurred while creating the card.`,
+        });
+        // return next(
+        // 	new ErrorResponse(`User not found with ID of: ${req.params.id}`, 404)
+        // );
+    }
+
+    res.status(200).json({ success: true, data: card });
+});
+
+export { getCard, createCard };
+
+

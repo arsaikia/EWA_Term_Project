8a1f766ce4de49d22ba96ef41dbce3686603e7ca
arsaikia <arunabh.matrix@gmail.com>
2020-11-29 21:46:25 -0600

    ...

--- backend/controller/marketbasket.js
+++ backend/controller/marketbasket.js
@@ -4,14 +4,10 @@ import asyncHandler from '../middleware/async.js';
 import ErrorResponse from '../middleware/error.js';
 import path from 'path';
 
-
 import data from '../_data/mba.json';
 // import JSON from ''
 
 /////
-
-
-
 
 /*
  * @desc     Get productB with productId
@@ -20,24 +16,32 @@ import data from '../_data/mba.json';
  */
 
 const getProduct = asyncHandler(async (req, res, next) => {
-    const product = await MarketBasket.findAll({
+    if (!req.body.products || req.body.products.length <= 0) {
+        return res.status(404).json({
+            success: false,
+            error: `Product to match cannot be empty`,
+        });
+    }
+
+    let product = await MarketBasket.findAll({
         where: {
-            productA: req.params.id,
+            productA: req.body.products,
         },
     });
 
     if (!product || product.length == 0) {
         console.log(`Product Not Found with user id ${req.params.id}`);
-        return res.status(404).json({
-            success: false,
-            error: `Product Not Found with user id '${req.params.id}'`,
+        return res.status(201).json({
+            success: true,
+            data: {},
         });
-        // return next(
-        // 	new ErrorResponse(`User not found with ID of: ${req.params.id}`, 404)
-        // );
     }
-    console.log(req.params.id);
-    return next(res.status(200).json({ success: true, data: product }));
+    product = JSON.parse(JSON.stringify(product));
+    const products = [];
+    product.forEach((product) => products.push(product.productB));
+
+    console.log('product', product);
+    return next(res.status(200).json({ success: true, data: products }));
 });
 
 /*
@@ -47,22 +51,21 @@ const getProduct = asyncHandler(async (req, res, next) => {
  */
 
 const createTable = asyncHandler(async (req, res, next) => {
-    
     // const pathName = fs.readFile(path.join(__dirname, '../_data', 'mba.json'), 'utf8', (err,data) => {
     //     if (err) throw err;
     //     console.log(data);
     // });
     const marketbasket = JSON.parse(JSON.stringify(data));
-    
+
     try {
         await MarketBasket.bulkCreate(marketbasket);
 
-        console.log('Data Imported...'.green.inverse);
-        process.exit();
+        console.log('Market basket reinitialized...'.green.inverse);
+        res.status(200).json({ success: true, data: marketbasket });
     } catch (err) {
         console.error(err);
+        res.status(404).json({ success: false, data: {} });
     }
-    res.status(200).json({ success: true, data: {} });
 });
 
 /*
